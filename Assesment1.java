ad96bbe50d61c5c2c45c9ca75d298fad54f3f99c
Manoj619Kumar <90104828+Manoj619Kumar@users.noreply.github.com>
2021-09-04 21:32:08 +0530

    Add files via upload

---
+++ Assesment1.java
@@ -0,0 +1,22 @@
+package MyFirstJavaProgram;
+import java.lang.*;
+
+public class Assesment1 {
+
+	public static void main(String[] args) {
+        int number= -40;
+        if(number<0) {
+        	System.out.println("The number is negative");
+        	System.out.println("The number -40 is converted to " + Math.abs(number));
+        	        }
+        else if(number>0) {
+        	System.out.println("The number positive");
+        	        }
+        else {
+        	System.out.println("The number is neither positive nor negative");
+        }
+       
+	}
+
+}
+//call me once u done with d call
---
+++ Assesment2.java
@@ -0,0 +1,19 @@
+package MyFirstJavaProgram;
+
+public class Assesment2 {
+
+	public static void main(String[] args) {
+		int number= -40;
+		if(number<0) {
+			System.out.println("The number is negative");
+		}
+		else if(number>0) {
+			System.out.println("The number is positive");
+		}
+		else {
+			System.out.println("The number is neither positive nor negative");
+		}
+
+	}
+
+}
